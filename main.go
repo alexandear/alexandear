c142c40fd77ab18326bb524457572e37f3708234
Oleksandr Redko <oleksandr.red+github@gmail.com>
2023-01-25 11:14:52 +0200

    Add contributions from golang org

--- main.go
+++ main.go
@@ -12,16 +12,27 @@ import (
 	"golang.org/x/oauth2"
 )
 
+type edgeComment struct {
+	Node struct {
+		Body githubv4.String
+	}
+}
+
 type edgePullRequest struct {
 	Node struct {
 		Repository struct {
 			NameWithOwner  githubv4.String
 			StargazerCount githubv4.Int
 		}
+		Comments struct {
+			Edges []edgeComment
+		} `graphql:"comments(last:1)"`
+		Merged githubv4.Boolean
+		Closed githubv4.Boolean
 	}
 }
 
-var queryMergedPRs struct {
+var queryPullRequest struct {
 	Viewer struct {
 		PullRequests struct {
 			PageInfo struct {
@@ -30,7 +41,7 @@ var queryMergedPRs struct {
 			}
 			TotalCount githubv4.Int
 			Edges      []edgePullRequest
-		} `graphql:"pullRequests(states:MERGED, orderBy:{field: CREATED_AT, direction: ASC}, first:100, after: $after)"`
+		} `graphql:"pullRequests(states: [MERGED, CLOSED], orderBy:{field: CREATED_AT, direction: ASC}, first:100, after: $after)"`
 	}
 }
 
@@ -51,27 +62,21 @@ func main() {
 	httpClient := oauth2.NewClient(context.Background(), src)
 	client := githubv4.NewClient(httpClient)
 
-	var allPullRequests []edgePullRequest
-	variables := map[string]any{
-		"after": (*githubv4.String)(nil),
+	allPullRequests, err := PullRequests(context.Background(), client)
+	if err != nil {
+		log.Fatalf("Failed to get merged pull requests: %v\n", err)
 	}
-	for {
-		if err := client.Query(context.Background(), &queryMergedPRs, variables); err != nil {
-			log.Fatalf("Executing query: %v", err)
-		}
-		allPullRequests = append(allPullRequests, queryMergedPRs.Viewer.PullRequests.Edges...)
-		if !queryMergedPRs.Viewer.PullRequests.PageInfo.HasNextPage {
-			break
-		}
-		variables["after"] = queryMergedPRs.Viewer.PullRequests.PageInfo.EndCursor
-	}
-
-	log.Printf("Total merged pull request: %d\n", len(allPullRequests))
+	log.Printf("Total pull request: %d\n", len(allPullRequests))
 
 	repositoryStars := map[string]int{}
 	for _, pr := range allPullRequests {
 		nameOwner := string(pr.Node.Repository.NameWithOwner)
-		if strings.HasPrefix(nameOwner, "alexandear") {
+		if ownRepo(nameOwner) {
+			log.Printf("Skipping own repo: %s\n", nameOwner)
+			continue
+		}
+		if !merged(pr) {
+			log.Printf("Skipping not merged repo: %s\n", nameOwner)
 			continue
 		}
 
@@ -104,10 +109,55 @@ func main() {
 
 	_, _ = contribFile.WriteString(`# Open Source Projects I've Ever Contributed
 
-The list of projects sorted desc by stars:
+The list of projects (with stars):
 
 `)
 	for _, repo := range repositories {
-		_, _ = contribFile.WriteString(fmt.Sprintf("* [%s](https://github.com/%s)\n", repo.OwnerName, repo.OwnerName))
+		_, _ = contribFile.WriteString(fmt.Sprintf("* [%s](https://github.com/%s) (%d)\n", repo.OwnerName, repo.OwnerName, repo.StarCount))
 	}
+}
+
+func PullRequests(ctx context.Context, client *githubv4.Client) ([]edgePullRequest, error) {
+	var pullRequests []edgePullRequest
+	variables := map[string]any{
+		"after": (*githubv4.String)(nil),
+	}
+
+	for {
+		if err := client.Query(ctx, &queryPullRequest, variables); err != nil {
+			return nil, fmt.Errorf("query: %w", err)
+		}
+		pullRequests = append(pullRequests, queryPullRequest.Viewer.PullRequests.Edges...)
+		if !queryPullRequest.Viewer.PullRequests.PageInfo.HasNextPage {
+			break
+		}
+		variables["after"] = queryPullRequest.Viewer.PullRequests.PageInfo.EndCursor
+	}
+
+	return pullRequests, nil
+}
+
+// ownRepo returns true if merged to my github.com/alexandear account.
+func ownRepo(nameOwner string) bool {
+	return strings.HasPrefix(nameOwner, "alexandear/")
+}
+
+func merged(pr edgePullRequest) bool {
+	return bool(pr.Node.Merged) || mergedToGolang(pr)
+}
+
+// mergedToGolang checks whether closed PR was merged to a repo in "golang" owner.
+// A merged PR to is closed by gopherbot if with the comment
+// "This PR is being closed because golang.org/cl/463098 has been merged."
+func mergedToGolang(pr edgePullRequest) bool {
+	if !strings.HasPrefix(string(pr.Node.Repository.NameWithOwner), "golang/") {
+		return false
+	}
+
+	comments := pr.Node.Comments.Edges
+	if len(comments) == 0 {
+		return false
+	}
+
+	return strings.Contains(string(comments[0].Node.Body), "has been merged")
 }
